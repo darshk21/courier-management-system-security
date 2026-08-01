4f6f3d48462ec6193b45a519195ac66e4ad304fa
shan <darshank70@gmail.com>
2026-08-01 09:23:54 +0800

    Add CSRF protection to customer forms

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+require_once __DIR__ . '/server/csrf.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php include 'pages/head.php'; ?>
@@ -144,6 +148,7 @@
 
 
           <form action="#" class="p-5 bg-white" method="post">
+            <?= csrfInputField(); ?>
 
 
             <div class="row form-group">
