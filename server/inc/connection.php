5b8301d3353e61151992fcd8adf002d56565a01b
Darshan Kumar <darshank70@gmail.com>
2026-08-12 16:53:16 +0800

    Prevent application error disclosure

--- server/inc/connection.php
+++ server/inc/connection.php
@@ -1,1 +1,20 @@
-<?php $con =  mysqli_connect("localhost", "root", "" , "royal_express_db") or die("Database Connection Fail"); ?>
+<?php
+
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '0');
+ini_set('log_errors', '1');
+error_reporting(E_ALL);
+
+$con = mysqli_connect(
+    'localhost',
+    'root',
+    '',
+    'royal_express_db'
+);
+
+if (!$con) {
+    error_log('Database connection failed: ' . mysqli_connect_error());
+
+    http_response_code(500);
+    exit('An internal server error occurred. Please try again later.');
+}
